122d35972cb06bbc3e9222b44ac5711d2b38d04e
Ivan5ik <lacoste545438@gmail.com>
2023-11-01 18:50:27 +0200

    without style

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Button.scss";
-import { CustomizeBtn } from "./style";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <CustomizeBtn>{props.label}</CustomizeBtn>;
+  return <button>{props.label}</button>;
 };
 
 export default Button;
