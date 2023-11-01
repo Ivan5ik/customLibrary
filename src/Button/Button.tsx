15daf7b76df3ec2e8b6bd16fa79a13cf2e6c15a6
Ivan5ik <lacoste545438@gmail.com>
2023-11-01 18:26:44 +0200

    almost component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Button.scss";
+import { CustomizeBtn } from "./style";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <CustomizeBtn>{props.label}</CustomizeBtn>;
 };
 
 export default Button;
---
+++ src/Button/style.ts
@@ -0,0 +1,10 @@
+import Btn from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+export const CustomizeBtn = styled(Btn)`
+  background-color: green;
+
+  :hover {
+    color: #2e8b57;
+  }
+`;
