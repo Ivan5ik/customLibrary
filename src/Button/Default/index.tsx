0c62bfae49c56c928b82217980c5a214bef6a5ec
Alex K <o.kostenko@infinitytechnologies.pro>
2023-11-21 15:05:55 +0200

    update btn

--- src/Button/Default/index.tsx
+++ src/Button/Default/index.tsx
@@ -1,36 +1,33 @@
 import React, { FC, ReactNode } from "react";
 import { ButtonProps as MuiButtonProps } from "@mui/material/Button";
 import { CustomizeBtn } from "./style";
-import { ThemeProvider } from "@mui/material";
 
 export interface DefaultButtonProps extends MuiButtonProps {
   className?: string;
   children: ReactNode;
-  variant: "contained" | "text";
+  // variant: "contained" | "text";
   // theme?: any;
 }
 
 const ButtonComponent: FC<DefaultButtonProps> = ({
   className,
   children,
-  variant = "contained",
+  // variant = "contained",
   // theme,
   ...props
 }) => {
-  const changeColor = (variant: string) =>
-    "contained" === variant ? true : false;
+  // const changeColor = (variant: string) =>
+  //   "contained" === variant ? true : false;
 
   return (
-    // <ThemeProvider theme={theme}>
     <CustomizeBtn
       {...props}
-      variant="contained"
-      variantColor={changeColor(variant)}
+      // variant="contained"
+      // variantColor={changeColor(variant)}
       className={className}
     >
       {children}
     </CustomizeBtn>
-    // </ThemeProvider>
   );
 };
 
--- src/Button/Default/style.ts
+++ src/Button/Default/style.ts
@@ -1,21 +1,23 @@
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-interface ElseColor {
-  variantColor: boolean;
-}
+// interface ElseColor {
+//   variantColor: boolean;
+// }
 
-export const CustomizeBtn = styled(Button)<ElseColor>(({ variantColor }) => ({
-  //   height: "40px",
-  //   background: variantColor ? "#2146AF" : "white",
-  //   boxShadow: "none",
-  //   textTransform: "none",
-  //   fontFamily: "Anek Latin, sans-serif",
-  //   color: variantColor ? "white" : "#2146AF",
-  //
-  //   ":hover": {
-  //     background: variantColor ? "#17327C" : "white",
-  //     color: variantColor ? "white" : "#17327C",
-  //     boxShadow: "none",
-  //   },
-}));
+export const CustomizeBtn = styled(Button)(
+  /*<ElseColor>*/ (/*{ variantColor }*/) => ({
+    //   height: "40px",
+    //   background: variantColor ? "#2146AF" : "white",
+    //   boxShadow: "none",
+    //   textTransform: "none",
+    //   fontFamily: "Anek Latin, sans-serif",
+    //   color: variantColor ? "white" : "#2146AF",
+    //
+    //   ":hover": {
+    //     background: variantColor ? "#17327C" : "white",
+    //     color: variantColor ? "white" : "#17327C",
+    //     boxShadow: "none",
+    //   },
+  })
+);
