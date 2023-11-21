dee71a5d381881c53be500f3c79d7de9f1b0ba0b
Alex K <o.kostenko@infinitytechnologies.pro>
2023-11-21 10:50:11 +0200

    update 27

--- src/TestButton/style.ts
+++ src/TestButton/style.ts
@@ -2,5 +2,6 @@ import Btn from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
 export const CustomizeBtn = styled(Btn)(({ theme }) => ({
-  background: theme.palette.primary.main,
+  // background: theme.palette.primary.main,
+  background: "green",
 }));
