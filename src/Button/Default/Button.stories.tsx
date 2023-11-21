582bc71cc4bd894e292b336c9629bac83ba9a9dc
Alex K <o.kostenko@infinitytechnologies.pro>
2023-11-21 14:08:05 +0200

    update v

--- src/Button/Default/Button.stories.tsx
+++ src/Button/Default/Button.stories.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { ButtonComponent } from ".";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 export default {
   title: "ReactComponentLibrary/Button",
   component: ButtonComponent,
 } as ComponentMeta<typeof ButtonComponent>;
 
-const Template: ComponentStory<typeof ButtonComponent> = (args) => (
-  <ButtonComponent {...args}>Search</ButtonComponent>
-);
+const Template: ComponentStory<typeof ButtonComponent> = (args) => {
+  return (
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          primary: {
+            main: "#0026ff",
+            dark: "#c51162",
+          },
+        },
+      })}
+    >
+      <ButtonComponent {...args}>Search</ButtonComponent>
+    </ThemeProvider>
+  );
+};
 export const Default = Template.bind({});
 Default.args = { className: "asd", variant: "contained" };
--- src/Button/Default/index.tsx
+++ src/Button/Default/index.tsx
@@ -1,23 +1,27 @@
 import React, { FC, ReactNode } from "react";
 import { ButtonProps as MuiButtonProps } from "@mui/material/Button";
 import { CustomizeBtn } from "./style";
+import { ThemeProvider } from "@mui/material";
 
 export interface DefaultButtonProps extends MuiButtonProps {
   className?: string;
   children: ReactNode;
   variant: "contained" | "text";
+  // theme?: any;
 }
 
 const ButtonComponent: FC<DefaultButtonProps> = ({
   className,
   children,
   variant = "contained",
+  // theme,
   ...props
 }) => {
   const changeColor = (variant: string) =>
     "contained" === variant ? true : false;
 
   return (
+    // <ThemeProvider theme={theme}>
     <CustomizeBtn
       {...props}
       variantColor={changeColor(variant)}
@@ -25,6 +29,7 @@ const ButtonComponent: FC<DefaultButtonProps> = ({
     >
       {children}
     </CustomizeBtn>
+    // </ThemeProvider>
   );
 };
 
